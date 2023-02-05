1b8f37c0702b8b64220b4668abfc903ca3b53d65
mohitverse <mohit@prabidhilabs.com>
2023-02-05 16:20:47 +0545

    Added dotenv

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.json());
